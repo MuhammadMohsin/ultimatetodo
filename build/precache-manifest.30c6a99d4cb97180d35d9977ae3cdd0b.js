0d6da55b4b891f31fdbddbdc3be37f9b1661de92
Muhammad Mohsin <mmohsinkhalid@outlook.com>
2018-11-27 19:55:13 +0500

    Added build file

---
+++ build/precache-manifest.30c6a99d4cb97180d35d9977ae3cdd0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "77fe57fd4ec1c7540452",
+    "url": "/static/js/main.77fe57fd.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "77fe57fd4ec1c7540452",
+    "url": "/static/css/main.5c5b74ff.chunk.css"
+  },
+  {
+    "revision": "c544aba9f5ea5563848d7a80973bfbf8",
+    "url": "/index.html"
+  }
+];
